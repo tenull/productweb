489eff567ee62a1c5f126686a9799e313a5808a3
Tamas  Nagy <tamas.nagy@vermis.hu>
2024-02-25 03:24:08 +0100

    2024.02.25

--- server/index.js
+++ server/index.js
@@ -1,30 +1,29 @@
-import dotenv from 'dotenv'
-dotenv.config()
-import connectToDatabase from './db.js'
-import express from 'express'
-import cors from 'cors'
+import dotenv from 'dotenv';
+dotenv.config();
+import connectToDatabase from './db.js';
+import express from 'express';
+import cors from 'cors';
 
+// Routes
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
-//Routes
-import productRoutes from './routes/productRoutes.js'
-import userRoutes from './routes/userRoutes.js'
+connectToDatabase();
+const app = express();
+app.use(express.json());
+app.use(cors());
 
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
-connectToDatabase()
-const app = express()
-app.use(express.json())
-app.use(cors())
-
-app.use('/api/products', productRoutes)
-app.use('/api/users', userRoutes)
-
+app.get('/api/config/google', (req, res) => res.send(process.env.GOOGLE_CLIENT_ID));
 
 const port = 5000;
 
-app.get('/', (req,res)=>{
-    res.send('Api is running...')
-})
+app.get('/', (req, res) => {
+	res.send('Api is running...');
+});
 
-app.listen(port,()=>{
-    console.log(`Server runs on port ${port}`)
-})
+app.listen(port, () => {
+	console.log(`Server runs on port ${port}`);
+});
--- server/middleware/sendPasswordResetEmail.js
+++ server/middleware/sendPasswordResetEmail.js
@@ -12,25 +12,26 @@ export const sendPasswordResetEmail = (token,email,name)=>{
     </body>
     </html>
     `
-    const transporter = nodemailer.createTransport({
-        service:'gmail',
-        auth:{
+	const transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
             user :'t0csa91@gmail.com',
             pass:'esbs okwt vbsy owtl'
         }
      })
      const mailOptions = {
-        from: 't0csa91@gmail.com',
-        to:email,
-        subject: 'Techlines: Reset your password request.',
-        html:html
-     }
-     transporter.sendMail(mailOptions,function(error,info){
-        if(error){
-            console.log(error)
-        } else {
-            console.log(`Email send to ${email}`)
-            console.log(info.response)
-        }
-     })
-}
+		from: 't0csa91@gmail.com',
+		to: email,
+		subject: 'Tech Lines: Reset your password request.',
+		html: html,
+	};
+
+	transporter.sendMail(mailOptions, function (error, info) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log(`Email send to ${email}`);
+			console.log(info.response);
+		}
+	});
+};
